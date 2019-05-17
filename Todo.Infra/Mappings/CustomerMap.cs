b9dfa53719e1a50c803eddfb4e3a0b834b052cfc
Willian Hideki <willhkblz@gmail.com>
2019-05-16 21:46:44 -0300

    Initial again

---
+++ Todo.Infra/Mappings/CustomerMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Infra.Models;
+
+namespace Todo.Infra.Mappings
+{
+    public class CustomerMap : EntityTypeConfiguration<Customer>
+    {
+        public CustomerMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Name).HasMaxLength(80);
+            HasMany(x => x.Items);
+        }
+    }
+}
---
+++ Todo.Infra/Mappings/TodoItemMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Infra.Models;
+
+namespace Todo.Infra.Mappings
+{
+    public class TodoItemMap : EntityTypeConfiguration<TodoItem>
+    {
+        public TodoItemMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Name).IsRequired().HasMaxLength(80);
+            Property(x => x.IsComplete);
+        }
+    }
+}
