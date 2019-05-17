9c1bcff86d6f973e4fa64eefac2d34941b64ff3f
Willian Hideki <willhkblz@gmail.com>
2019-05-16 22:41:35 -0300

    Finalizando com problemas 16-05

---
+++ Todo.Infra/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Todo.Infra.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Todo.Infra.Context.TodoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Todo.Infra.Context.TodoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Todo.Infra.Context;
 
 namespace TodoApi
 {
@@ -15,6 +16,8 @@ namespace TodoApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<TodoContext, TodoContext>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
